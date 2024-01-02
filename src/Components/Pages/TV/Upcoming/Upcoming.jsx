600196f9543c31c1693f3dd164145ecb289c4229
Hryhorii Shkvyra <52300830+HryhoriiShkvyra@users.noreply.github.com>
2024-01-02 16:39:26 +0200

    Movie page looks like close to be done

---
+++ src/Components/Pages/TV/Upcoming/Upcoming.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import "./Upcoming.css";
+import CardPage from "../../../CardPageFolder/CardPage/CardPage";
+import Sort from "../../../Filters/Sort/Sort";
+import WhereToWatch from "../../../Filters/Where to watch/WhereToWatch";
+import Filters from "../../../Filters/Filters/Filters";
+import Card from "../../../Card/Card";
+import MovieWrapper from "../../../MovieWrapper/MovieWrapper";
+import { useLocation } from "react-router-dom";
+
+export default function Upcoming() {
+  const pageState = "Upcoming";
+
+  const [data, setData] = React.useState([
+    {
+      title: "Sort",
+      filterState: false,
+      filterSubState: false,
+      btnTitle: "Sort",
+      listTitle: "Sort Results By",
+      list: [
+        "Popularity Descending",
+        "Popularity Ascending",
+        "Rating Descending",
+        "Rating Ascending",
+        "Release Date Descending",
+        "Release Date Ascending",
+        "Title (A-Z)",
+        "Title (Z-A)",
+      ],
+    },
+    {
+      title: "Where to watch",
+      filterState: false,
+      filterSubState: false,
+      btnTitle: "Where to watch",
+      listTitle: "My Services",
+      checkbox: "Restrict searches to my subscribed services?",
+      list: [
+        "Ukraine 1",
+        "Ukraine 2",
+        "Ukraine 3",
+        "Ukraine 4",
+        "Ukraine 5",
+        "Ukraine 6",
+        "Ukraine 7",
+        "Ukraine 8",
+      ],
+    },
+  ]);
+  const [movieState, setMovieState] = React.useState();
+  const [handleGenres, setHandleGenres] = React.useState();
+
+  const handleClickFilterState = (title) => {
+    setData(
+      data.map((item) =>
+        item.title === title
+          ? { ...item, filterState: !item.filterState }
+          : item
+      )
+    );
+  };
+
+  const handleClickFilterSubState = (listTitle) => {
+    setData(
+      data.map((item) =>
+        item.listTitle === listTitle
+          ? { ...item, filterSubState: !item.filterSubState }
+          : item
+      )
+    );
+  };
+
+  console.log(pageState);
+
+  return (
+    <div className="movie">
+      {/* dsadasd */}
+      <MovieWrapper pageState={pageState} />
+      {/* <MTVPage /> */}
+    </div>
+  );
+}
