beb90f49d98a1c2653ce50d867f3dd4b4b0de55b
Hryhorii Shkvyra <52300830+HryhoriiShkvyra@users.noreply.github.com>
2024-05-10 22:03:14 +0300

    initial commit

--- src/Components/CardPageFolder/CardPageAdditionalInfo/CardPageAdditionalInfo.jsx
+++ src/Components/CardPageFolder/CardPageAdditionalInfo/CardPageAdditionalInfo.jsx
@@ -8,6 +8,8 @@ import {
 import { useParams } from "react-router-dom";
 import Loading from "../../Loading/Loading";
 import PersonRoundedIcon from "@mui/icons-material/PersonRounded";
+import ArrowDropDownRoundedIcon from "@mui/icons-material/ArrowDropDownRounded";
+import ArrowRightRoundedIcon from "@mui/icons-material/ArrowRightRounded";
 
 export default function CardPageAdditionalInfo({ requestType }) {
   const { id } = useParams();
@@ -25,12 +27,16 @@ export default function CardPageAdditionalInfo({ requestType }) {
     console.log(main_url);
   }, []);
 
-  const [cardValue, setCardValue] = React.useState([]);
+  const [cardData, setCardData] = React.useState([]);
+  const [matrixCollection, setMatrixCollection] = React.useState();
   const [isLoading, setIsLoading] = React.useState(true);
   const [networksArray, setNetworksArray] = React.useState();
   const [keywordsArray, setKeywordsArray] = React.useState();
   const [featuredCast, setFeaturedCast] = React.useState();
   const [fightClub, setFightClub] = React.useState();
+
+  const [sortBtn, setSortBtn] = React.useState(false);
+  const [sortSubBtn, setSortSubBtn] = React.useState(false);
 
   const fetch = require("node-fetch");
 
@@ -47,6 +53,14 @@ export default function CardPageAdditionalInfo({ requestType }) {
     "https://api.themoviedb.org/3/movie/603/credits?language=en-US";
 
   const url = "https://api.themoviedb.org/3/movie/550?language=en-US";
+  const the_matrix_url =
+    "https://api.themoviedb.org/3/movie/603?language=en-US";
+  const the_matrix_reload_url =
+    "https://api.themoviedb.org/3/movie/604?language=en-US";
+  const the_matrix_revolutions_url =
+    "https://api.themoviedb.org/3/movie/605?language=en-US";
+  const the_matrix_resurrection_url =
+    "https://api.themoviedb.org/3/movie/624860?language=en-US";
 
   const options = {
     method: "GET",
@@ -73,7 +87,7 @@ export default function CardPageAdditionalInfo({ requestType }) {
         const data = request;
 
         console.log(data);
-        setCardValue(data[0]);
+        setCardData(data[0]);
         setKeywordsArray(data[1].keywords);
       }
       if (requestType === "collection") {
@@ -92,18 +106,45 @@ export default function CardPageAdditionalInfo({ requestType }) {
           response.json()
         );
 
+        const the_matrix_promise = fetch([the_matrix_url], options).then(
+          (response) => response.json()
+        );
+
+        const the_matrix_reload_promise = fetch(
+          [the_matrix_reload_url],
+          options
+        ).then((response) => response.json());
+
+        const the_matrix_revolution_promise = fetch(
+          [the_matrix_revolutions_url],
+          options
+        ).then((response) => response.json());
+
+        const the_matrix_resurrection_promise = fetch(
+          [the_matrix_resurrection_url],
+          options
+        ).then((response) => response.json());
+
         // const request = Promise.all([collections_promise]);
         const request = await Promise.all([
           collections_promise,
           featuredCast_promise,
           fightClub_promise,
           url_promise,
+          the_matrix_promise,
+          the_matrix_reload_promise,
+          the_matrix_revolution_promise,
+          the_matrix_resurrection_promise,
         ]);
         const data = request;
 
+        const matrix_collection = [data[4], data[5], data[6], data[7]];
+        setMatrixCollection(matrix_collection);
+
         console.log(data);
 
-        setCardValue(data[3]);
+        setCardData(data[3]);
+        // setCardData(data[3]);
         setFightClub(data[2].cast);
       }
     } catch (error) {
@@ -145,6 +186,14 @@ export default function CardPageAdditionalInfo({ requestType }) {
     // }
   };
 
+  const Movie_overview_limit = (overview) => {
+    const newOverview = overview.slice(0, 250) + "...";
+
+    if (overview.length > 250) {
+      return newOverview;
+    } else return overview;
+  };
+
   const CardPageAdditionalInfoType = () => {
     if (requestType === "movie") {
       return <CardPageAdditionalInfoMovie />;
@@ -154,6 +203,10 @@ export default function CardPageAdditionalInfo({ requestType }) {
       return <CardPageAdditionalInfoCollection />;
     } else return console.log("error");
   };
+
+  React.useEffect(() => {
+    console.log(sortBtn);
+  }, [sortBtn]);
 
   const CardPageAdditionalInfoMovie = () => {
     return (
@@ -172,25 +225,25 @@ export default function CardPageAdditionalInfo({ requestType }) {
             <div className="card-additional-info-col">
               <h4>status</h4>
               <div className="card-additional-info-second">
-                {cardValue.status}
+                {cardData.status}
               </div>
             </div>
             <div className="card-additional-info-col">
               <h4>original language</h4>
               <div className="card-additional-info-second">
-                {cardValue.original_language}
+                {cardData.original_language}
               </div>
             </div>
             <div className="card-additional-info-col">
               <h4>budget</h4>
               <div className="card-additional-info-second">
-                ${cardValue.budget}.00
+                ${cardData.budget}.00
               </div>
             </div>
             <div className="card-additional-info-col">
               <h4>revenue</h4>
               <div className="card-additional-info-second">
-                ${cardValue.revenue}.00
+                ${cardData.revenue}.00
               </div>
             </div>
           </div>
@@ -279,26 +332,24 @@ export default function CardPageAdditionalInfo({ requestType }) {
             <div className="card-additional-info-col">
               <h4>facts</h4>
               <div className="card-additional-info-second">
-                {/* {cardValue.status} */}
+                {/* {cardData.status} */}
               </div>
             </div>
             <div className="card-additional-info-col">
               <h4>original name</h4>
               <div className="card-additional-info-second">
-                {cardValue.original_name}
+                {cardData.original_name}
               </div>
             </div>
             {/* <NetworksLength /> */}
             <div className="card-additional-info-col">
               <h4>type</h4>
-              <div className="card-additional-info-second">
-                {cardValue.type}
-              </div>
+              <div className="card-additional-info-second">{cardData.type}</div>
             </div>
             <div className="card-additional-info-col">
               <h4>original language</h4>
               <div className="card-additional-info-second">
-                {cardValue.original_language}
+                {cardData.original_language}
               </div>
             </div>
           </div>
@@ -445,29 +496,159 @@ export default function CardPageAdditionalInfo({ requestType }) {
         <div className="card-page-black-blank"></div>
 
         <div className="card-page-section">
-          <h2>Featured Cast</h2>
-          <div className="card-page-firms">
-            <div className="card-page-film">
-              <div className="card-page-film-poster-wrapper">
-                <img
-                  className="card-page-film-poster"
-                  src={
-                    process.env.REACT_APP_IMAGE_URL +
-                    "w200" +
-                    `${cardValue.poster_path}`
+          <div className="card-page-title-wrapper">
+            <h2>{matrixCollection.length} movies</h2>
+            <div
+              onMouseEnter={(e) => setSortBtn(true)}
+              onMouseLeave={(e) => setSortBtn(false)}
+              className="card-page-film-sort"
+            >
+              <div className="card-page-sort-title">
+                <h2>Sort</h2>
+                <ArrowDropDownRoundedIcon />
+              </div>
+              <div className="card-page-sort-btn">
+                <div
+                  className={
+                    sortBtn
+                      ? "card-page-sort-cols-on"
+                      : "card-page-sort-cols-off"
                   }
-                />
-              </div>
-              <div className="card-page-film-text">
-                <div className="card-page-film-title">
-                  <h2>{cardValue.original_title}</h2>
-                  <p>{cardValue.release_date}</p>
+                >
+                  <div className="card-page-sort-cols-wrapper">
+                    <div
+                      onMouseEnter={(e) => setSortSubBtn(true)}
+                      onMouseLeave={(e) => setSortSubBtn(false)}
+                      className="card-page-sort-col"
+                    >
+                      <span className="card-page-sort-col-title">
+                        Popularity
+                        <ArrowRightRoundedIcon />
+                      </span>
+                      <div className="card-page-sort-sub-cols-wrapper">
+                        <div
+                          className={
+                            sortSubBtn
+                              ? "card-page-sort-sub-cols-on"
+                              : "card-page-sort-sub-cols-off"
+                          }
+                        >
+                          <div className="card-page-sort-sub-col">
+                            Ascending
+                          </div>
+                          <div className="card-page-sort-sub-col">
+                            Descending
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div
+                      onMouseEnter={(e) => setSortSubBtn(true)}
+                      onMouseLeave={(e) => setSortSubBtn(false)}
+                      className="card-page-sort-col"
+                    >
+                      <span className="card-page-sort-col-title">Rating</span>
+                      <ArrowRightRoundedIcon />
+                    </div>
+                    <div
+                      onMouseEnter={(e) => setSortSubBtn(true)}
+                      onMouseLeave={(e) => setSortSubBtn(false)}
+                      className="card-page-sort-col"
+                    >
+                      <span className="card-page-sort-col-title">
+                        Released Date
+                      </span>
+                      <ArrowRightRoundedIcon />
+                    </div>
+                  </div>
+                  {/* <div className="card-page-sort-sub-cols-wrapper">
+                    <div
+                      className={
+                        sortSubBtn
+                          ? "card-page-sort-sub-cols-on"
+                          : "card-page-sort-sub-cols-off"
+                      }
+                    >
+                      <div className="card-page-sort-sub-col">Ascending</div>
+                      <div className="card-page-sort-sub-col">Descending</div>
+                    </div>
+                  </div> */}
                 </div>
-                <div className="card-page-film-about">
-                  <h3>{cardValue.overview}</h3>
-                </div>
               </div>
+
+              {/* <div
+                onMouseLeave={(e) => setSortBtn(false)}
+                className="card-page-sort-cols"
+              >
+                <div
+                  onMouseEnter={(e) => setSortBtn(true)}
+                  className="card-page-sort-cols-title"
+                >
+                  Sort
+                  <ArrowDropDownRoundedIcon />
+                </div>
+                <div
+                  onMouseEnter={(e) => setSortSubBtn(true)}
+                  onMouseLeave={(e) => setSortSubBtn(false)}
+                  className={
+                    sortBtn
+                      ? "card-page-sort-cols-show"
+                      : "card-page-sort-cols-hidden"
+                  }
+                >
+                  <ol>
+                    <li className="card-page-sort-col-title">
+                      Popularity <ArrowRightRoundedIcon />
+                    </li>
+                  </ol>
+                  
+                </div>
+                <div
+                  className={
+                    sortSubBtn
+                      ? "card-page-sort-sub-cols-show"
+                      : "card-page-sort-sub-cols-hidden"
+                  }
+                >
+                  <ol
+                  className={
+                    sortSubBtn
+                      ? "card-page-sort-sub-cols-show"
+                      : "card-page-sort-sub-cols-hidden"
+                  }
+                  >
+                    <li>Ascending</li>
+                    <li>Descending</li>
+                  </ol>
+                </div>
+              </div> */}
             </div>
+          </div>
+          <div className="card-page-films">
+            {matrixCollection.map((movie, index) => (
+              <div key={`${movie.id}-${index}`} className="card-page-film">
+                <div className="card-page-film-poster-wrapper">
+                  <img
+                    className="card-page-film-poster"
+                    src={
+                      process.env.REACT_APP_IMAGE_URL +
+                      "w200" +
+                      `${movie.poster_path}`
+                    }
+                  />
+                </div>
+                <div className="card-page-film-text">
+                  <div className="card-page-film-title">
+                    <h2>{movie.original_title}</h2>
+                    <p>{movie.release_date}</p>
+                  </div>
+                  <div className="card-page-film-about">
+                    {/* <h3>{movie.overview}</h3> */}
+                    {Movie_overview_limit(movie.overview)}
+                  </div>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
